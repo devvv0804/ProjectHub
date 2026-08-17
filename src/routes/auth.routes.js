1189c0c262fb4e8633bc0e0c70a746cbcbb86cf7
Dev Srivastava <devsrivastava080405@gmail.com>
2026-08-17 19:51:00 +0530

    added validators

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/auth.controllers.js";
-const router = Router();
 
-router.route("/register").post(registerUser);
+import { validate } from "../middlewares/validator.middleware.js";
+
+import { userRegisterValidator } from "../validators/index.js";
+
+const router = Router();
+router.route("/register").post(userRegisterValidator(), validate, registerUser); //we run "()" userRegisterValidator but not validate!! important since its a middleware that takes care of the errors userRegisterValidators finds
 export default router;
